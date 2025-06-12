8c4939601c6713ad4329e824a49808f28c5458f1
Deylin <deylinbo91@gmail.com>
2025-06-11 22:54:33 -0600

    Actualizar fake.js

--- plugins/fake.js
+++ plugins/fake.js
@@ -1,3 +1,4 @@
+// 📦 Importaciones
 import pkg from '@whiskeysockets/baileys'
 import fs from 'fs'
 import fetch from 'node-fetch'
@@ -8,6 +9,7 @@ const { generateWAMessageFromContent, prepareWAMessageMedia, proto } = pkg
 var handler = m => m
 handler.all = async function (m) {
 
+// 🌐 Función Global para obtener Buffers
 global.getBuffer = async function getBuffer(url, options) {
 try {
 options ? options : {}
@@ -27,23 +29,24 @@ return res.data
 console.log(`Error : ${e}`)
 }}
 
-//creador y otros
+// 👑 Información del Bot
 global.creador = 'Wa.me/50433191934'
 global.ofcbot = `${conn.user.jid.split('@')[0]}`
 global.asistencia = 'Wa.me/50433191934'
-global.namechannel = '࿋ོ༙𝐊𝐢𝐫𝐢𝐭𝐨-𝑩𝒐𝒕 𝑴𝑫 𝐂𝐡𝐚𝐧𝐧𝐞𝐥࿐ཽ༵'
-global.namechannel2 = '᭄𝐊𝐢𝐫𝐢𝐭𝐨-𝐁𝐨𝐭 𝐌𝐃 👑ᬊᬁ"'
-global.namegrupo = '𝑲𝒊𝒓𝒊𝒕𝒐-𝑩𝒐𝒕'
-global.namecomu = 'ᬊ𝑲𝒊𝒓𝒊𝒕𝒐 𝑩𝒐𝒕 𝑴𝑫 𝑪𝒐𝒎𝒖𝒏𝒊𝒕𝒚ᬊ᭄'
-global.listo = '⚡ *ᴀǫᴜɪ ᴛɪᴇɴᴇs ˙Ⱉ˙ฅ*'
+global.namechannel = '⚡️𝙋𝙞𝙠𝙖𝙘𝙝𝙪-𝘽𝙤𝙩 ⚡ 𝘾𝙝𝙖𝙣𝙣𝙚𝙡'
+global.namechannel2 = '⚡️𝙋𝙞𝙠𝙖𝙘𝙝𝙪-𝘽𝙤𝙩 ⚡'
+global.namegrupo = '⚡ 𝙋𝙞𝙠𝙖𝙘𝙝𝙪-𝘽𝙤𝙩 ⚡'
+global.namecomu = '⚡️𝙋𝙞𝙠𝙖𝙘𝙝𝙪-𝘽𝙤𝙩 ⚡ 𝘾𝙤𝙢𝙪𝙣𝙞𝙩𝙮'
+global.listo = '⚡ *¡Aquí tienes lo que pediste, maestro pokémon!*'
 global.fotoperfil = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://i.postimg.cc/RFdNynN5/IMG-20250315-WA0122.jpg')
 
+// 🗞 Canal del Bot
 global.idchannel = '120363365444927738@newsletter'
 global.canalIdM = ["120363365444927738@newsletter", "120363365444927738@newsletter"]
-global.canalNombreM = [" ๖ۣ•҉𝑲𝒊𝒓𝒊𝒕𝒐-𝑩𝒐𝒕 𝑴𝑫 𝑪𝒉𝒂𝒏𝒏𝒆𝒍★᭄", "᭄𝑲𝒊𝒓𝒊𝒕𝒐-𝑩𝒐𝒕 𝑴𝑫ᬊ᭄"]
+global.canalNombreM = ["⚡️𝙋𝙞𝙠𝙖𝙘𝙝𝙪-𝘽𝙤𝙩 ⚡ 𝘾𝙝𝙖𝙣𝙣𝙚𝙡", "⚡️𝙋𝙞𝙠𝙖𝙘𝙝𝙪-𝘽𝙤𝙩 ⚡"]
 global.channelRD = await getRandomChannel()
 
-//fechas
+// 📆 Fechas y Hora
 global.d = new Date(new Date + 3600000)
 global.locale = 'es'
 global.dia = d.toLocaleDateString(locale, {weekday: 'long'})
@@ -52,37 +55,36 @@ global.mes = d.toLocaleDateString('es', {month: 'long'})
 global.año = d.toLocaleDateString('es', {year: 'numeric'})
 global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true})
 
-//Reacciones De Comandos.!
-global.rwait = '🕒'
+// 🔁 Reacciones Globales
+global.rwait = '⚡'
 global.done = '✅'
 global.error = '✖️'
-global.msm = '⚠︎'
+global.msm = '⚠️'
 
-global.emoji0 = '*❛‿˂̵✧*'
-global.emoji2 = '*（＾∀＾●）ﾉｼ*'
-global.emoji3 = '*ฅ°ㅅ°ฅ*'
+global.emoji0 = '*⚡*'
+global.emoji2 = '*(≧▽≦)*'
+global.emoji3 = '*(^・ω・^ )*'
 global.emoji4 = '*ฅ՞•ﻌ•՞ฅ*'
-global.emoji5 = '*˙Ⱉ˙ฅ*'
+global.emoji5 = '*⚡˙Ⱉ˙ฅ*'
 global.emoji = [emoji0, emoji2, emoji3, emoji4, emoji5].getRandom()
 
-//mensaje en espera
-global.wait = '*˙Ⱉ˙ฅ* *𝗘𝘀𝗽𝗲𝗿𝗮 𝗨𝗻 𝗠𝗼𝗺𝗲𝗻𝘁𝗼, 𝗦𝗼𝘆 𝗟𝗲𝗻𝘁𝗼 ...*';
-global.waitt = '*˙Ⱉ˙ฅ* *𝗘𝘀𝗽𝗲𝗿𝗮 𝗨𝗻 𝗠𝗼𝗺𝗲𝗻𝘁𝗼, 𝗦𝗼𝘆 𝗟𝗲𝗻𝘁𝗼 ...*';
-global.waittt = '*ฅ՞•ﻌ•՞ฅ* *𝗘𝘀𝗽𝗲𝗿𝗮 𝗨𝗻 𝗠𝗼𝗺𝗲𝗻𝘁𝗼, 𝗦𝗼𝘆 𝗟𝗲𝗻𝘁𝗼 ...*';
-global.waitttt = '*ฅ՞•ﻌ•՞ฅ* *𝗘𝘀𝗽𝗲𝗿𝗮 𝗨𝗻 𝗠𝗼𝗺𝗲𝗻𝘁𝗼, 𝗦𝗼𝘆 𝗟𝗲𝗻𝘁𝗼 ...*';
+global.wait = '⚡ *Espera un momento entrenador...*'
+global.waitt = global.wait
+global.waittt = global.wait
+global.waitttt = global.wait
 
-global.code = 'https://chat.whatsapp.com/IjpqfWuWYVfG9cDgR0AnKn';
+global.code = 'https://chat.whatsapp.com/IjpqfWuWYVfG9cDgR0AnKn'
 
-//Enlaces
+// 🔗 Enlaces Importantes
 var canal = 'https://whatsapp.com/channel/0029VawF8fBBvvsktcInIz3m'  
-let canal2 = 'https://whatsapp.com/channel/0029VawF8fBBvvsktcInIz3m'
+let canal2 = canal
 var git = 'https://github.com/Deylin-Eliac'
 var github = 'https://github.com/Deylin-Eliac/Kirito-Bot-MD' 
 let correo = 'deylibaquedano801@gmail.com'
 
 global.redes = [canal, canal2, git, github, correo].getRandom()
 
-//Imagen
+// 🖼 Imagen Aleatoria
 let category = "imagen"
 const db = './src/database/db.json'
 const db_ = JSON.parse(fs.readFileSync(db))
@@ -92,39 +94,86 @@ const response = await fetch(randomlink)
 const rimg = await response.buffer()
 global.icons = rimg
 
-//• ↳ ◜𝑻𝑰𝑬𝑴𝑷𝑶 𝑹𝑷𝑮◞ • ⚔
-var ase = new Date(); var hour = ase.getHours(); switch(hour){ case 0: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 1: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 2: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 3: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 4: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 5: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 6: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 7: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌅'; break; case 8: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 9: hour = 'Lɪɴᴅᴀ Mᴀɴ̃ᴀɴᴀ 🌄'; break; case 10: hour = 'Lɪɴᴅᴏ Dɪᴀ 🌤'; break; case 11: hour = 'Lɪɴᴅᴏ Dɪᴀ 🌤'; break; case 12: hour = 'Lɪɴᴅᴏ Dɪᴀ 🌤'; break; case 13: hour = 'Lɪɴᴅᴏ Dɪᴀ 🌤'; break; case 14: hour = 'Lɪɴᴅᴀ Tᴀʀᴅᴇ 🌆'; break; case 15: hour = 'Lɪɴᴅᴀ Tᴀʀᴅᴇ 🌆'; break; case 16: hour = 'Lɪɴᴅᴀ Tᴀʀᴅᴇ 🌆'; break; case 17: hour = 'Lɪɴᴅᴀ Tᴀʀᴅᴇ 🌆'; break; case 18: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 19: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 20: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 21: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 22: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break; case 23: hour = 'Lɪɴᴅᴀ Nᴏᴄʜᴇ 🌃'; break;}
-global.saludo = hour;
+// 🌇 Mensaje según la hora
+var ase = new Date(); var hour = ase.getHours(); switch(hour){
+case 0:
+case 1:
+case 2:
+case 18:
+case 19:
+case 20:
+case 21:
+case 22:
+case 23:
+hour = 'Buenas noches 🌙'; break
+case 3:
+case 4:
+case 5:
+case 6:
+case 7:
+case 8:
+case 9:
+hour = 'Buenos días 🌅'; break
+case 10:
+case 11:
+case 12:
+case 13:
+hour = 'Buen día ☀️'; break
+case 14:
+case 15:
+case 16:
+case 17:
+hour = 'Buenas tardes 🌆'; break
+}
+global.saludo = hour
 
-//tags
-global.nombre = m.pushName || 'Anónimo'
+// 🧍 Tags
+global.nombre = m.pushName || 'Entrenador'
 global.taguser = '@' + m.sender.split("@s.whatsapp.net")
 var more = String.fromCharCode(8206)
 global.readMore = more.repeat(850)
 
-//Fakes
-let pp = null; try { pp = await conn.profilePictureUrl('50433191934@s.whatsapp.net', 'image') } catch (e) { pp = null } global.fkontak = { key: {participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `120363402481697721@g.us` } : {}) }, message: { 'contactMessage': { 'displayName': `Deylin creador ✨`, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;Deylin creador✨,;;;\nFN:Deylin creador\nitem1.TEL;waid=50433191934:50433191934\nitem1.X-ABLabel:Ponsel\nEND:VCARD`, 'jpegThumbnail': pp ? await (await fetch(pp)).buffer() : null, thumbnail: null, sendEphemeral: true }}}
+// 🧾 Fake Contacto
+let pp = null; try { pp = await conn.profilePictureUrl('50433191934@s.whatsapp.net', 'image') } catch (e) { pp = null } 
+global.fkontak = { key: {participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `120363402481697721@g.us` } : {}) }, 
+message: { 'contactMessage': { 'displayName': `Deylin ✨`, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;Deylin✨,;;;\nFN:Deylin\nitem1.TEL;waid=50433191934:50433191934\nitem1.X-ABLabel:Ponsel\nEND:VCARD`, 'jpegThumbnail': pp ? await (await fetch(pp)).buffer() : null, thumbnail: null, sendEphemeral: true }}}
 
-global.fake = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, newsletterName: channelRD.name, serverMessageId: -1 }
-}}, { quoted: m }
+global.fake = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, newsletterName: channelRD.name, serverMessageId: -1 }}}, { quoted: m }
 
-
-global.icono = [ 
+global.icono = [
 'https://i.postimg.cc/RFdNynN5/IMG-20250315-WA0122.jpg',
-'https://i.postimg.cc/RFdNynN5/IMG-20250315-WA0122.jpg',
-'https://i.postimg.cc/RFdNynN5/IMG-20250315-WA0122.jpg',
-'https://i.postimg.cc/3JjzVC0N/IMG-20250318-WA0969.jpg',
-'https://i.postimg.cc/3JjzVC0N/IMG-20250318-WA0969.jpg',
 'https://i.postimg.cc/3JjzVC0N/IMG-20250318-WA0969.jpg'
 ].getRandom()
 
-global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: 100, newsletterName: channelRD.name, }, externalAdReply: { showAdAttribution: true, title: packname, body: dev, mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: icono, sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, }, }}
+global.rcanal = {
+contextInfo: {
+isForwarded: true,
+forwardedNewsletterMessageInfo: {
+newsletterJid: channelRD.id,
+serverMessageId: 100,
+newsletterName: channelRD.name,
+},
+externalAdReply: {
+showAdAttribution: true,
+title: '⚡ Pikachu-Bot MD ⚡',
+body: 'Creado por Deylin',
+mediaUrl: null,
+description: null,
+previewType: "PHOTO",
+thumbnailUrl: icono,
+sourceUrl: redes,
+mediaType: 1,
+renderLargerThumbnail: false
+},
+}
+}
 
 export default handler
 
+// 🔁 Funciones auxiliares
 function pickRandom(list) {
 return list[Math.floor(Math.random() * list.length)]
-  }
+}
 
 async function getRandomChannel() {
 let randomIndex = Math.floor(Math.random() * canalIdM.length)
