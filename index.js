d3e2788d9cb4ae754a65e6bbb9ab83155f43d509
Deylin <deylinbo91@gmail.com>
2025-06-11 23:10:23 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -212,7 +212,7 @@ if (opcion == '1' || methodCodeQR) {
 console.log(chalk.bold.yellow(`\n❐ ESCANEA EL CÓDIGO QR EXPIRA EN 45 SEGUNDOS`))}
 }
 if (connection == 'open') {
-console.log(chalk.bold.green('\n👑 kirit-Bot-MD Conectada con éxito 👑'))
+console.log(chalk.bold.green('\n⚡ Pikachu Conectada con éxito 🧃'))
 }
 let reason = new Boom(lastDisconnect?.error)?.output?.statusCode
 if (connection === 'close') {
