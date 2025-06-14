f29141719286d966f4fbc0d1eb2aefad8259755c
Angelbr-spc <angelroos.carbajal@gmail.com>
2025-06-14 17:25:19 -0600

    Actualizar downloader-spotify.js

--- plugins/downloader-spotify.js
+++ plugins/downloader-spotify.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) throw m.reply(`⚡ Por favor, ingresa el nombre de una canción de Spotify.`);
+if (!text) throw m.reply(`⚡ Por favor, ingresa el nombre de una canción de Spotify., m, rcanal);`
 await m.react('🕒');
 let ouh = await fetch(`https://api.nekorinn.my.id/downloader/spotifyplay?q=${text}`)
 let gyh = await ouh.json()
