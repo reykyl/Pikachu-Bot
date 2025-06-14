083563b8073cf7e8d413456b0acb7b4761e63326
Deylin <deylinbo91@gmail.com>
2025-06-14 17:33:12 -0600

    Crear _antisubs.js

---
+++ plugins/_antisubs.js
@@ -0,0 +1,32 @@
+import { areJidsSameUser } from '@whiskeysockets/baileys'
+
+export async function before(m, { participants, conn }) {
+    if (m.isGroup) {
+        let chat = global.db.data.chats[m.chat];
+
+        if (!chat.antiBot2) {
+            return
+        }
+
+        let botJid = global.conn.user.jid // JID del bot principal
+
+        if (botJid === conn.user.jid) {
+            return
+        } else {
+            let isBotPresent = participants.some(p => areJidsSameUser(botJid, p.id))
+
+            if (isBotPresent) {
+                setTimeout(async () => {
+                    await conn.reply(m.chat, 
+`⚡──〔 🐭 *PIKACHU-BOT* ⚡ 〕──⚡
+¡Pika! ⚠️ Ya estoy en este grupo como *bot principal*.
+
+Para evitar spam y confusión, me voy tranquilamente 🌩️
+¡Hasta luego, entrenador!
+─────────────────────────`, m, rcanal)
+                    await this.groupLeave(m.chat)
+                }, 5000) 
+            }
+        }
+    }
+}
