49aea002281678b8fc935b7cc5f3248b2c412b09
Deylin <deylinbo91@gmail.com>
2025-06-14 23:27:25 -0600

    Actualizar ia-mode.js

--- plugins/ia-mode.js
+++ plugins/ia-mode.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
-var handler = async (m, { text, usedPrefix, command }) => {
-  if (!text) return conn.reply(m.chat, `${emoji} Ingrese una petición para que Mode IA lo responda.`, m, rcanal)
+var handler = async (m, { text, usedPrefix, command, args }) => {
+  if (!text) return conn.reply(m.chat, `✨ Ingrese una petición para que Mode IA lo responda.`, m)
   try {
     await m.react('✨')
     conn.sendPresenceUpdate('composing', m.chat)
@@ -13,7 +13,7 @@ var handler = async (m, { text, usedPrefix, command }) => {
     await m.reply(data.response.trim())
   } catch (e) {
     await m.react('🌟')
-    await conn.reply(m.chat, `${emoji} Mode IA no puede responder a esa pregunta.`, m, rcanal)
+    await conn.reply(m.chat, `💢 Mode IA no puede responder a esa pregunta.`, m)
   }
 }
 
@@ -21,5 +21,10 @@ handler.command = ['modeia', 'mode', 'ia']
 handler.help = ['modeia']
 handler.tags = ['ai']
 handler.group = true
+
+
+handler.customPrefix = /^(@ia|ia|modeia|mode)/i
+handler.command = new RegExp 
+handler.nonPrefix = true 
 
 export default handler
