e5c4a72b3e6a69e18451278e02a90edc468ad6d6
Deylin <deylinbo91@gmail.com>
2025-06-14 22:09:04 -0600

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -559,8 +559,8 @@ function pickRandom(list) { return list[Math.floor(Math.random() * list.length)]
 global.dfail = (type, m, conn) => {
 
   let edadaleatoria = ['10', '28', '20', '40', '18', '21', '15', '11', '9', '17', '25'].getRandom();
-  let user2 = m.pushName || 'Anónimo';
-  let verifyaleatorio = ['registrar', 'reg', 'verificar', 'verify', 'register'].getRandom();
+  /*let user2 = m.pushName || 'Anónimo';
+  let verifyaleatorio = ['registrar', 'reg', 'verificar', 'verify', 'register'].getRandom();*/
 
   const msg = {
   rowner: `*〘 ${comando} 〙 es una función exclusiva de los propietarios principales. Tu acceso no está autorizado.*`,
@@ -571,7 +571,7 @@ global.dfail = (type, m, conn) => {
   private: `*〘 ${comando} 〙 debe utilizarse en un chat privado. Intenta de nuevo en el canal adecuado.*`,
   admin: `*〘 ${comando} 〙 requiere permisos de administrador. Acceso denegado.*`,
   botAdmin: `*Para ejecutar 〘 ${comando} 〙, el bot necesita ser administrador. Por favor, actualiza los permisos.*`,
-  unreg: `*Para usar 〘 ${comando} 〙 primero debes registrarte.*\n\n*Utiliza:* _#${verifyaleatorio} ${user2}.${edadaleatoria}_`,
+ // unreg: `*Para usar 〘 ${comando} 〙 primero debes registrarte.*\n\n*Utiliza:* _#${verifyaleatorio} ${user2}.${edadaleatoria}_`,
   restrict: `*Esta función está desactivada. No se permiten excepciones.*`
 }[type];
 
