b4a8fa2d9c9b27348847c6ccda18a0ba88d623ca
Deylin <deylinbo91@gmail.com>
2025-06-15 07:58:29 -0600

    Actualizar stiker-stiker.js

--- plugins/stiker-stiker.js
+++ plugins/stiker-stiker.js
@@ -38,7 +38,7 @@ else return m.reply(`${emoji} El url es incorrecto`)
 console.error(e)
 if (!stiker) stiker = e
 } finally {
-if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: packname, body: `⏤͟͞𝐊𝐢𝐫𝐢𝐭𝐨 𝐁𝐨𝐭 𝐌𝐃✰⃔`, mediaType: 2, sourceUrl: redes, thumbnail: icons}}}, { quoted: m })
+if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: packname, body: `Pikachu-Bot 🧃`, mediaType: 2, sourceUrl: redes, thumbnail: icons}}}, { quoted: m })
 
 else return conn.reply(m.chat, `${emoji} *_La conversión ha fallado, intenta enviar primero imagen/video/gif y luego responde con el comando._*`, m, rcanal)
 
