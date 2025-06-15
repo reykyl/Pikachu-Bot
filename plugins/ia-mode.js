54eb9f0060f196bc402c4a0d91273a3f71ccf865
Deylin <deylinbo91@gmail.com>
2025-06-14 23:56:41 -0600

    Actualizar ia-mode.js

--- plugins/ia-mode.js
+++ plugins/ia-mode.js
@@ -1,40 +1,29 @@
 import fetch from 'node-fetch'
 
-var handler = async (m, { text, usedPrefix, command }) => {
-  
-  const body = m.text || ''
-  const prefixRegex = /^[@.]/ 
-  const commandRegex = /^(modeia|mode)/i
-
-  
-  if (!prefixRegex.test(body) && !commandRegex.test(body)) return
-  if (!commandRegex.test(body.slice(1))) return
-
-  let prompt = body.replace(prefixRegex, '').replace(commandRegex, '').trim()
-  if (!prompt) return conn.reply(m.chat, `🤖 Ingrese una petición para que Mode IA lo responda.`, m)
-
+var handler = async (m, { text, usedPrefix, command, args }) => {
+  if (!text) return conn.reply(m.chat, `✨ Ingrese una petición para que Mode IA lo responda.`, m)
   try {
-    await m.react('🌟')
+    await m.react('✨')
     conn.sendPresenceUpdate('composing', m.chat)
 
-    let response = await fetch(`https://mode-ia.onrender.com/mode-ia?prompt=${encodeURIComponent(prompt)}`)
+    let response = await fetch(`https://mode-ia.onrender.com/mode-ia?prompt=${encodeURIComponent(text)}`)
     let data = await response.json()
 
     if (!data.response) throw 'Sin respuesta válida'
     await m.reply(data.response.trim())
   } catch (e) {
-    await m.react('⚡️')
-    await conn.reply(m.chat, `🤖 Mode IA no puede responder a esa pregunta.`, m)
+    await m.react('🌟')
+    await conn.reply(m.chat, `💢 Mode IA no puede responder a esa pregunta.`, m)
   }
 }
 
-
-handler.command = ['modeia', 'mode']
+/*handler.command = ['modeia', 'mode', 'ia']
 handler.help = ['modeia']
 handler.tags = ['ai']
-handler.group = true
-handler.customPrefix = /^[@.](modeia|mode)/i
-handler.before = async (m) => true 
-handler.prefix = false 
+handler.group = true*/
+
+handler.customPrefix = /^(@ia|.ia|@modeia|.modeia|@mode|.mode|)/i
+handler.command = new RegExp 
+handler.nonPrefix = true 
 
 export default handler
