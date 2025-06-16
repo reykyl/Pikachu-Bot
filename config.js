0a30898cf94d6223711fdfbc1af3bccbdcdcb952
Angelbr-spc <angelroos.carbajal@gmail.com>
2025-06-15 22:45:19 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -29,7 +29,7 @@ global.confirmCode = ''
 // ⚙️ PROPIETARIO Y STAFF
 global.owner = [
 ['50433191934', '🔰 Creador 🔰', true],
-['5215649896365', 'Angel', true],
+['5217227584934', 'Angel', true],
 ['573001533523', 'BʀᴀʏᴀɴXD', true],
 //['numero', 'nombre', true],
 ];
