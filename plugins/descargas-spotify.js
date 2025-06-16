7eb30d158e8cbe145757a04cdfa5412a5044b69d
Deylin <deylinbo91@gmail.com>
2025-06-15 21:44:38 -0600

    Actualizar descargas-spotify.js

--- plugins/descargas-spotify.js
+++ plugins/descargas-spotify.js
@@ -29,7 +29,15 @@ let gyh = await ouh.json()
       };
 
 
-      return await conn.sendMessage(m.chat, doc, { quoted: m });
+     await conn.sendMessage(m.chat, { audio: { url: gyh.result.downloadUrl }, externalAdReply: {
+            showAdAttribution: true,
+            mediaType: 2,
+            mediaUrl: url,
+            title: title,
+            body: `Duración: ${timestamp} | Vistas: ${vistas}`,
+            sourceUrl: url,
+            thumbnailUrl: "https://raw.githubusercontent.com/Deylin-Eliac/Pikachu-Bot/refs/heads/main/src/IMG-20250613-WA0194.jpg",
+            renderLargerThumbnail: true, mimetype: 'audio/mpeg' }, { quoted: m });
 await m.react('✅');
 }
 handler.help = ['spotify *<texto>*']
@@ -37,6 +45,26 @@ handler.tags = ['descargas']
 handler.command = ['spotify']
 
 export default handler
+
+
+
+
+/*import fetch from 'node-fetch'
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+if (!text) throw m.reply(`⚡ Por favor, ingresa el nombre de una canción de Spotify.`);
+await m.react('🕒');
+let ouh = await fetch(`https://api.nekorinn.my.id/downloader/spotifyplay?q=${text}`)
+let gyh = await ouh.json()
+
+await conn.sendMessage(m.chat, { audio: { url: gyh.result.downloadUrl }, mimetype: 'audio/mpeg' }, { quoted: m });
+await m.react('✅');
+}
+handler.help = ['spotify *<texto>*']
+handler.tags = ['descargas']
+handler.command = ['spotify']
+
+export default handler*)
 
 
 
