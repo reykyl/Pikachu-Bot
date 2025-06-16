958256d5d0e5090bc5f6a33b3dd129f4233f151a
Deylin <deylinbo91@gmail.com>
2025-06-15 21:13:56 -0600

    Actualizar descargas-spotify.js

--- plugins/descargas-spotify.js
+++ plugins/descargas-spotify.js
@@ -1,2 +1,19 @@
-//no muevas, estoy haciendo uno.
-// a la próxima ponle//
+/*Hecho por Angel brou*/
+
+
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+if (!text) return conn.reply(m.chat, ⚡ Por favor, ingresa el nombre de una canción de Spotify.`, m, rcanal);
+await m.react('🕒');
+let ouh = await fetch(`https://api.nekorinn.my.id/downloader/spotifyplay?q=${text}`)
+let gyh = await ouh.json()
+
+await conn.sendMessage(m.chat, { audio: { url: gyh.result.downloadUrl }, mimetype: 'audio/mpeg' }, { quoted: m });
+await m.react('✅');
+}
+handler.help = ['spotify *<texto>*']
+handler.tags = ['descargas']
+handler.command = ['spotify']
+
+export default handler
