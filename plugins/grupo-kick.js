787d26bcfbe8e9ee80908b103d7b376732569c90
Angelbr-spc <angelroos.carbajal@gmail.com>
2025-06-16 03:40:43 -0600

    Actualizar grupo-kick.js

--- plugins/grupo-kick.js
+++ plugins/grupo-kick.js
@@ -3,7 +3,7 @@ var handler = async (m, { conn, participants, usedPrefix, command }) => {
     const sadchu = 'Ｏ(≧∇≦)Ｏ🧃';
 
     if (!m.mentionedJid[0] && !m.quoted) {
-        return conn.reply(m.chat, `${pikachu} ¡Pika Pika! Debes mencionar a alguien para expulsarlo del grupo.`, m, rcanal);
+        return conn.reply(m.chat, `${pikachu} ¡Pika Pika Debes mencionar a alguien para expulsarlo del grupo.`, m, rcanal);
     }
 
     let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender;
