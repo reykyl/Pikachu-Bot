157689245372af5a5728e517a288e355e52285be
Deylin <deylinbo91@gmail.com>
2025-06-15 21:48:23 -0600

    Actualizar descargas-spotify.js

--- plugins/descargas-spotify.js
+++ plugins/descargas-spotify.js
@@ -10,34 +10,25 @@ return conn.reply(m.chat, `*Espera un momento estoy buscando...*`, m, rcanal);
 let ouh = await fetch(`https://api.nekorinn.my.id/downloader/spotifyplay?q=${text}`)
 let gyh = await ouh.json()
 
-      /*const doc = {
-        audio: { url: gyh.result.downloadUrl },
-        mimetype: 'audio/mpeg',
-        fileName: `${title}.mp3`,
-        contextInfo: {
-          externalAdReply: {
-            showAdAttribution: true,
-            mediaType: 2,
-            mediaUrl: url,
-            title: title,
-            body: `Duración: ${timestamp} | Vistas: ${vistas}`,
-            sourceUrl: url,
-            thumbnailUrl: "https://raw.githubusercontent.com/Deylin-Eliac/Pikachu-Bot/refs/heads/main/src/IMG-20250613-WA0194.jpg",
-            renderLargerThumbnail: true
-          }
-        }
-      };*/
+      const doc = {
+  audio: { url: gyh.result.downloadUrl },
+  mimetype: 'audio/mpeg',
+  fileName: `${title}.mp3`,
+  contextInfo: {
+    externalAdReply: {
+      showAdAttribution: true,
+      mediaType: 2,
+      mediaUrl: url,
+      title: title,
+      body: `Duración: ${timestamp} | Vistas: ${vistas}`,
+      sourceUrl: url,
+      thumbnailUrl: "https://raw.githubusercontent.com/Deylin-Eliac/Pikachu-Bot/refs/heads/main/src/IMG-20250613-WA0194.jpg",
+      renderLargerThumbnail: true
+    }
+  }
+};
 
-
-     await conn.sendMessage(m.chat, { audio: { url: gyh.result.downloadUrl }, externalAdReply: {
-            showAdAttribution: true,
-            mediaType: 2,
-            mediaUrl: url,
-            title: title,
-            body: `Duración: ${timestamp} | Vistas: ${vistas}`,
-            sourceUrl: url,
-            thumbnailUrl: "https://raw.githubusercontent.com/Deylin-Eliac/Pikachu-Bot/refs/heads/main/src/IMG-20250613-WA0194.jpg",
-            renderLargerThumbnail: true, mimetype: 'audio/mpeg' }, { quoted: m });
+await conn.sendMessage(m.chat, doc, { quoted: m });
 await m.react('✅');
 }
 handler.help = ['spotify *<texto>*']
