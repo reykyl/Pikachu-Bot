6c0ccd4f19f30183b41538ad6c0acdbc6e003e4e
Deylin <deylinbo91@gmail.com>
2025-06-16 00:11:57 -0600

    Actualizar sticker-brat.js

--- plugins/sticker-brat.js
+++ plugins/sticker-brat.js
@@ -24,7 +24,7 @@ let handler = async (m, { conn, text, command, usedPrefix }) => {
     
 
     if (!text) {
-        return conn.reply(m.chat, `${emojis} *Uso incorrecto del comando.*\n\n📌 Ejemplo:\n${usedPrefix + command} ${botname}`, m);
+        return conn.reply(m.chat, `${emojis} *Uso incorrecto del comando.*\n\n*Ejemplo:*${usedPrefix + command} ${botname}`, m, rcanal);
     }
 
     try {
