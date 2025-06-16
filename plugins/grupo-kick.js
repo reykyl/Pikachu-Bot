24bb2d9ca5ab6ff78baf760af5712c64ac6501b8
Angelbr-spc <angelroos.carbajal@gmail.com>
2025-06-16 01:36:10 -0600

    Actualizar grupo-kick.js

--- plugins/grupo-kick.js
+++ plugins/grupo-kick.js
@@ -29,7 +29,7 @@ var handler = async (m, { conn, participants, usedPrefix, command }) => {
 
 handler.help = ['kick'];
 handler.tags = ['grupo'];
-handler.command = ['kick','echar','hechar','sacar',' kick'];
+handler.command = ['kick','echar','hechar','sacar','ban'];
 handler.admin = true;
 handler.group = true;
 handler.register = true;
