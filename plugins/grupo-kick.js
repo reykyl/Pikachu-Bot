b121c72e9f80e0bbde1d49202e1094c9b9fa875d
Angelbr-spc <angelroos.carbajal@gmail.com>
2025-06-16 17:45:56 -0600

    Actualizar grupo-kick.js

--- plugins/grupo-kick.js
+++ plugins/grupo-kick.js
@@ -1,31 +1,30 @@
 var handler = async (m, { conn, participants, usedPrefix, command }) => {
-    
+    const pikachu = 'Ｏ(≧∇≦)Ｏ🧃';
+    const sadchu = 'Ｏ(≧∇≦)Ｏ🧃';
 
     if (!m.mentionedJid[0] && !m.quoted) {
-        return conn.reply(m.chat, `${emojis} ¡Pika! Debes mencionar a un entrenador para expulsarlo del grupo.`, m, rcanal);
+        return conn.reply(m.chat, `${pikachu} ¡Pika Pika! Debes mencionar a alguien para expulsarlo del grupo.`, m, rcanal);
     }
 
     let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender;
-
     const groupInfo = await conn.groupMetadata(m.chat);
     const ownerGroup = groupInfo.owner || m.chat.split`-`[0] + '@s.whatsapp.net';
     const ownerBot = global.owner[0][0] + '@s.whatsapp.net';
 
     if (user === conn.user.jid) {
-        return conn.reply(m.chat, `${emojis} ¡Pikachu no se puede autoeliminar!`, m, rcanal);
+        return conn.reply(m.chat, `${sadchu} ¡Pika! No puedo eliminarme a mí mismo.`, m, rcanal);
     }
 
     if (user === ownerGroup) {
-        return conn.reply(m.chat, `${emojis} ¡Ese es el maestro Pokémon del grupo, no puedo expulsarlo!`, m, rcanal);
+        return conn.reply(m.chat, `${sadchu} ¡Pikachu no se mete con el líder del grupo!`, m, rcanal);
     }
 
     if (user === ownerBot) {
-        return conn.reply(m.chat, `${emojis} ¡Ese es el dueño del bot Pikachu, no puedo atacarlo!`, m, rcanal);
+        return conn.reply(m.chat, `${sadchu} ¡Ese es mi entrenador! No puedo hacer eso.`, m, rcanal);
     }
 
     await conn.groupParticipantsUpdate(m.chat, [user], 'remove');
-
-    conn.reply(m.chat, `${emojis} ¡Pika Pika! Un entrenador ha sido expulsado del gimnasio.`, m, rcanal);
+    conn.reply(m.chat, `${pikachu} ¡Pika Pika! Usuario eliminado con un Impactrueno.`, m, rcanal);
 };
 
 handler.help = ['kick'];
