48738a2a6ee853b9cfb661814d8ceadd4227e591
Angelbr-spc <angelroos.carbajal@gmail.com>
2025-06-16 00:51:07 -0600

    Actualizar sticker-qc.js

--- plugins/sticker-qc.js
+++ plugins/sticker-qc.js
@@ -10,7 +10,7 @@ text = args.slice(0).join(" ");
 } else if (m.quoted && m.quoted.text) {
 text = m.quoted.text;
 } else return conn.reply(m.chat, `⚡ Te Faltó El Texto!`, m rcanal);
-if (!text) return conn.reply(m.chat, `⚡ Te Faltó El Texto!`, m);
+if (!text) return conn.reply(m.chat, `⚡ Te Faltó El Texto!`, m rcanal);
 const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender; 
 const mentionRegex = new RegExp(`@${who.split('@')[0].replace(/[.*+?^${}()|[\]\\]/g, '\\$&')}\\s*`, 'g');
 const mishi = text.replace(mentionRegex, '');
