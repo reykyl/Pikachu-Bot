a54f6585790f4965386ba43b38bb2c081f5ddcef
Angelbr-spc <angelroos.carbajal@gmail.com>
2025-06-16 01:05:02 -0600

    Actualizar sticker-qc.js

--- plugins/sticker-qc.js
+++ plugins/sticker-qc.js
@@ -14,7 +14,7 @@ if (!text) return conn.reply(m.chat, `⚡ Te Faltó El Texto!`, m, rcanal);
 const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender; 
 const mentionRegex = new RegExp(`@${who.split('@')[0].replace(/[.*+?^${}()|[\]\\]/g, '\\$&')}\\s*`, 'g');
 const mishi = text.replace(mentionRegex, '');
-if (mishi.length > 40) return conn.reply(m.chat, `${emoji2} El texto no puede tener mas de 30 caracteres`, m);
+if (mishi.length > 40) return conn.reply(m.chat, `*ᰔᩚ ( ᜊ°-° )ᜊ  .ᐟ .ᐟ 🧃* _*Entrenador! El texto no puede tener más de 30 palabras*_`, m, rcanal);
 const pp = await conn.profilePictureUrl(who).catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
 const nombre = await conn.getName(who)
 const obj = {"type": "quote", "format": "png", "backgroundColor": "#000000", "width": 512, "height": 768, "scale": 2, "messages": [{"entities": [], "avatar": true, "from": {"id": 1, "name": `${who?.name || nombre}`, "photo": {url: `${pp}`}}, "text": mishi, "replyMessage": {}}]};
