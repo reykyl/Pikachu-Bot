7fe16de39bce35c51396f22a0d0403176c7436d3
Deylin <deylinbo91@gmail.com>
2025-06-16 20:53:39 -0600

    Actualizar info-ping.js

--- plugins/info-ping.js
+++ plugins/info-ping.js
@@ -1,9 +1,11 @@
 import speed from 'performance-now'
 import { exec } from 'child_process'
+import ws from 'ws'
 
 let handler = async (m, { conn }) => {
   let timestamp = speed();
   let latensi = speed() - timestamp;
+  const users = [...new Set([...global.conns.filter((conn) => conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED).map((conn) => conn)])];
 
   exec(`neofetch --stdout`, (error, stdout, stderr) => {
     let sysinfo = stdout.toString("utf-8").replace(/Memory:/, "Ram:");
@@ -12,7 +14,7 @@ let handler = async (m, { conn }) => {
 ╭━━━⊰ ⚡ *Pikachu-Bot* ⚡ ⊱━━━╮
 ┃ ⚡ *Estado:* ¡Activo y cargado! ⚡
 ┃ 🕒 *Velocidad:* ${latensi.toFixed(4)} ms
-┃ 📊 *Sistema:*
+┃ 
 ┃
 ╰━━━━━━━━━━━━━━━━━━━━━━━╯
 `.trim();
