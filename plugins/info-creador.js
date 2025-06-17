75a7fa4cbc564a4c290f6f75cd745ebc7b1bd346
Deylin <deylinbo91@gmail.com>
2025-06-16 21:12:27 -0600

    Crear info-creador.js

---
+++ plugins/info-creador.js
@@ -0,0 +1,50 @@
+// Código creado por Deylin
+// https://github.com/deylinqff
+// No quites créditos
+
+import PhoneNumber from 'awesome-phonenumber';
+
+async function handler(m, { conn }) {
+  m.react('🧃');
+  const numCreador = '50433191934';
+  const ownerJid = numCreador + '@s.whatsapp.net';
+
+  const name = await conn.getName(ownerJid) || 'Deylin';
+  const about = (await conn.fetchStatus(ownerJid).catch(() => {}))?.status || `Hola, mucho gusto. Soy Deylin.`;
+  const empresa = 'Deylin - Servicios Tecnológicos';
+
+
+  const vcard = `
+BEGIN:VCARD
+VERSION:3.0
+N:;${name};;;
+FN:${name}
+ORG:${empresa};
+TITLE:CEO & Fundador
+TEL;waid=${numCreador}:${new PhoneNumber('+' + numCreador).getNumber('international')}
+EMAIL:correo@empresa.com
+URL:https://www.tuempresa.com
+NOTE:${about}
+ADR:;;Dirección de tu empresa;;;;
+X-ABADR:ES
+X-ABLabel:Dirección Web
+X-ABLabel:Correo Electrónico
+X-ABLabel:Teléfono de contacto
+X-WA-BIZ-NAME:${name}
+X-WA-BIZ-DESCRIPTION:${about}
+END:VCARD
+  `.trim();
+
+
+  await conn.sendMessage(
+    m.chat,
+    { contacts: { displayName: name, contacts: [{ vcard }] } },
+    { quoted: fkontak }
+  );
+}
+
+handler.help = ['owner'];
+handler.tags = ['main'];
+handler.command = ['owner', 'creator', 'creador', 'dueño'];
+
+export default handler;
