293f52da3ecc4dd908a0cb3310b4e148a44fd40a
Deylin <deylinbo91@gmail.com>
2025-06-18 00:46:57 -0600

    Actualizar main.js

--- main.js
+++ main.js
@@ -147,7 +147,7 @@ const connectionOptions = {
 logger: pino({ level: 'silent' }),
 printQRInTerminal: opcion == '1' ? true : methodCodeQR ? true : false,
 mobile: MethodMobile, 
-browser: opcion == '1' ? ['Vegeta-Super-Bot', 'Edge', '20.0.04'] : methodCodeQR ? ['Vegeta-Super-Bot', 'Edge', '20.0.04'] : ["Ubuntu", "Chrome", "20.0.04"],
+browser: opcion == '1' ? ['pikachu-Bot', 'Edge', '20.0.04'] : methodCodeQR ? ['pikachu-Bot', 'Edge', '20.0.04'] : ["Ubuntu", "Chrome", "20.0.04"],
 auth: {
 creds: state.creds,
 keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "fatal" }).child({ level: "fatal" })),
