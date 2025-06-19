be83bbbcdda80bff0d7e12c6b13837ed2659adf2
Deylin <deylinbo91@gmail.com>
2025-06-18 20:56:04 -0600

    Actualizar ia-image-generator.js

--- plugins/ia-image-generator.js
+++ plugins/ia-image-generator.js
@@ -34,8 +34,8 @@ const handler = async (m, { conn, args }) => {
     }
 };
 
-handler.command = ['imgg', 'pikaia', 'generarimg', 'genera', 'arte'];
-handler.help = ['imgg <descripción>', 'kiritoia <descripción>'];
+handler.command = ['imgg', 'pikaia', 'generarimg', 'genera', 'arte', 'img', 'ai', 'imgai'];
+handler.help = ['imgg <texto>', 'pikaia', 'generarimg', 'genera', 'arte', 'img', 'ai', 'imgai'];
 handler.tags = ['tools'];
 
 export default handler;
