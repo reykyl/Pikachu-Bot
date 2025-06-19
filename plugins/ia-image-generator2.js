88e9bc0cad8b22181966d603126db1c7a033c8fb
Deylin <deylinbo91@gmail.com>
2025-06-19 00:37:37 -0600

    Actualizar ia-image-generator2.js

--- plugins/ia-image-generator2.js
+++ plugins/ia-image-generator2.js
@@ -9,7 +9,7 @@ let handler = async (m, { text, conn }) => {
 
 
   if (!text) {
-    return await conn.reply(m.chat, `${emojis} Escribe el prompt de la imagen. Ejemplo:\n.genera un dragón azul volando en el espacio`, m, rcanal)
+    return await conn.reply(m.chat, `${emojis} Escribe el prompt de la imagen. Ejemplo:\n.imagina un dragón azul volando en el espacio`, m, rcanal)
   }
 
   await conn.reply(m.chat, `${emojis} Generando imagen de: "${text}", espera un momento...`, m, rcanal)
@@ -25,8 +25,8 @@ let handler = async (m, { text, conn }) => {
   }
 }
 
-handler.help = ['genera <prompt>']
-handler.tags = ['ai']
+handler.help = ['imagina <prompt>']
+handler.tags = ['ia'];
 handler.command = ['imgrg', 'imagina']
 
 export default handler
