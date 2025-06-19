fa2129a897fc03c224be5b94ca11535180b3a999
Deylin <deylinbo91@gmail.com>
2025-06-18 23:56:56 -0600

    Actualizar ia-image-generator2.js

--- plugins/ia-image-generator2.js
+++ plugins/ia-image-generator2.js
@@ -1,14 +1,14 @@
 let handler = async (m, { text, conn }) => {
-  if (!text) await conn.reply(m.chat, `✏️ Escribe el prompt de la imagen. Ejemplo:\n.genera un dragón azul volando en el espacio`, m, rcanal);
+  if (!text) await conn.reply(m.chat, `${emojis} Escribe el prompt de la imagen. Ejemplo:\n.genera un dragón azul volando en el espacio`, m, rcanal);
 
-  await conn.reply(m.chat, `🪄 Generando imagen, espera un momento...', m, rcanal);
+  await conn.reply(m.chat, `${emojis} Generando imagen de (${text}), espera un momento...', m, rcanal);
 
   try {
     
     let prompt = encodeURIComponent(text.trim())
     let imageUrl = `https://anime-xi-wheat.vercel.app/api/ia-img?prompt=${prompt}`
 
-    await conn.sendFile(m.chat, imageUrl, 'imagen.jpg', `🖼️ Imagen generada:\n"${text}"`, m, rcanal)
+    await conn.sendFile(m.chat, imageUrl, 'imagen.jpg', `${emojis} Imagen generada:\n"${text}"`, m, rcanal)
   } catch (e) {
     console.error(e)
     m.reply(`❌ Ocurrió un error al generar la imagen:\n${e.message}`)
