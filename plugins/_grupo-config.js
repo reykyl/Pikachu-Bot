b3d9be23b256a939f5d23e52f96cefd15a5fd36e
Deylin <deylinbo91@gmail.com>
2025-06-18 19:29:27 -0600

    Actualizar _grupo-config.js

--- plugins/_grupo-config.js
+++ plugins/_grupo-config.js
@@ -1,32 +1,29 @@
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-  const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => icono) 
+const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => icono) 
+let isClose = { // Switch Case Like :v
+'open': 'not_announcement',
+'close': 'announcement',
+'abierto': 'not_announcement',
+'cerrado': 'announcement',
+'on': 'not_announcement',
+'off': 'announcement',
+}[(args[0] || '')]
+if (isClose === undefined)
+return conn.reply(m.chat, `⚡️🐭 *¡Pika Pika! Elige una opción para configurar el grupo*\n\nEjemplos:\n*⚡ ${usedPrefix + command} on*\n*⚡ ${usedPrefix + command} off*\n*⚡ ${usedPrefix + command} close*\n*⚡ ${usedPrefix + command} open*`, m, rcanal)
+await conn.groupSettingUpdate(m.chat, isClose)
 
-  let isClose = {
-    'open': 'not_announcement',
-    'close': 'announcement',
-    'abierto': 'not_announcement',
-    'cerrado': 'announcement',
-    'on': 'not_announcement',
-    'off': 'announcement',
-  }[(args[0] || '').toLowerCase()]
-
-  if (isClose === undefined)
-    return conn.reply(m.chat, `⚡️🐭 *¡Pika Pika! Elige una opción para configurar el grupo*\n\nEjemplos:\n*⚡ ${usedPrefix + command} on*\n*⚡ ${usedPrefix + command} off*\n*⚡ ${usedPrefix + command} close*\n*⚡ ${usedPrefix + command} open*`, m, rcanal)
-
-  await conn.groupSettingUpdate(m.chat, isClose)
-
-  if (isClose === 'not_announcement') {
-    m.reply(`🗨️✨ *¡Pikachu dice que todos pueden hablar ahora!*\n\n⚡ ¡Pikapi! El grupo ha sido abierto.`)
-  }
-
-  if (isClose === 'announcement') {
-    const m.reply(m.chat, `🔒🚫 *¡Pikachu activó el modo silencioso!*\n\n⚡ Solo los *líderes de gimnasio* (admins) pueden hablar ahora.` m, rcanal);
-  }
+if (isClose === 'not_announcement'){
+   const m.reply(m.chat, `🗨️✨ *¡Pikachu dice que todos pueden hablar ahora!*\n\n⚡ ¡Pikapi! El grupo ha sido abierto.` m, rcanal)
 }
+
+if (isClose === 'announcement'){
+const m.reply(m.chat, `🔒🚫 *¡Pikachu activó el modo silencioso!*\n\n⚡ Solo los *líderes de gimnasio* (admins) pueden hablar ahora.` m, rcanal)
+}}
 handler.help = ['group open / close', 'grupo on / off']
 handler.tags = ['grupo']
 handler.command = ['group', 'grupo']
 handler.admin = true
-handler.botAdmin = true
+handler.botAdmin = true;
+
 
 export default handler
