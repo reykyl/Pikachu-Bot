826cc4af0cad1dc74cc16158e4ea6e585c1f8383
Deylin <deylinbo91@gmail.com>
2025-06-19 18:59:27 -0600

    Actualizar tools-hd.js

--- plugins/tools-hd.js
+++ plugins/tools-hd.js
@@ -4,7 +4,7 @@ const handler = async (m, {conn, usedPrefix, command}) => {
  try {    
   let q = m.quoted ? m.quoted : m;
   let mime = (q.msg || q).mimetype || q.mediaType || "";
-  if (!mime) return m.reply(`${emojis} Envie una imagen o responda a la imagen utilizando el comando: ${usedPrefix + command}`);
+  if (!mime) return m.reply(`${emojis} Envie una imagen o responda a la imagen utilizando el comando: /hd `);
   if (!/image\/(jpe?g|png)/.test(mime)) return m.reply(`${emojis} El formato del archivo (${mime}) no es compatible, envía o responda a una imagen`);
   conn.reply(m.chat, '(●’∇’)ノ Mejorando la calidad de la imagen....', m, {
   contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
