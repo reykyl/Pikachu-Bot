c517133b0282052d67caf6856868460aa5143bcd
Deylin <deylinbo91@gmail.com>
2025-06-20 19:50:30 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -2,5 +2,27 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 return conn.reply(m.chat,`*"APIs desarrolladas por Deylin"*
 
 [1]
+https://anime-xi-wheat.vercel.app/api/pinterest?q=
+> pinterest: buscador de imágenes en Pinterest.
+
+[2]
+https://anime-xi-wheat.vercel.app/api/ia-img?prompt=
+> generador de imágenes con IA.
+
+[3]
+https://g-mini-ia.vercel.app/api/gemini
+> IA gemini responde preguntas, analiza imágenes, genera imágenes.
+
+[4]
+https://mode-ia.onrender.com/mode-ia?prompt=
+> mode-ia inteligencia artificial para responder preguntas solo en texto.
+
+[5]
+https://Ytumode-api.vercel.app/api/search?q=
+> busca contenido de YouTube.
+
+[6]
+https://mode-api-sigma.vercel.app/api/mp3?url=
+> descarga audio de YouTube.
 `, m, rcanal);
 
