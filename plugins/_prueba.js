91023d5c21535edd96958586babfa3dff81c15ce
Deylin <deylinbo91@gmail.com>
2025-06-21 22:40:17 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -1,7 +1,7 @@
 const handler = async (m, { conn }) => {
   const taguser = `@${m.sender.split("@")[0]}`;
 
-  // O puedes hacer un saludo según la hora:
+  
   const hour = new Date().getHours();
   const saludo = hour >= 5 && hour < 12 ? "buenos días" :
                  hour >= 12 && hour < 18 ? "buenas tardes" :
