6f55fbd0c75bac49b902706ccf80f8b1e013b64f
Deylin <deylinbo91@gmail.com>
2025-06-21 22:38:52 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -1,6 +1,16 @@
 const handler = async (m, { conn }) => {
-      //const taguser = `@${who.split("@")[0]}`;
-return conn.reply(m.chat, `hola ${global.taguser} ${global.saludo}`, m)};
+  const taguser = `@${m.sender.split("@")[0]}`;
+
+  // O puedes hacer un saludo según la hora:
+  const hour = new Date().getHours();
+  const saludo = hour >= 5 && hour < 12 ? "buenos días" :
+                 hour >= 12 && hour < 18 ? "buenas tardes" :
+                 "buenas noches";
+
+  return conn.reply(m.chat, `Hola ${taguser}, ${saludo} 🌟`, m, {
+    mentions: [m.sender]
+  });
+};
 
 handler.command = ['hola'];
 
