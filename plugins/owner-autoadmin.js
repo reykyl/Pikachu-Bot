83f58d1998e51b352a330f3074fa9df3b9d83967
Deylin <deylinbo91@gmail.com>
2025-06-22 18:21:05 -0600

    Actualizar owner-autoadmin.js

--- plugins/owner-autoadmin.js
+++ plugins/owner-autoadmin.js
@@ -1,11 +1,11 @@
 const handler = async (m, {conn, isAdmin, groupMetadata }) => {
-  if (isAdmin) return m.reply(`${emoji} Tu ya eres admin.`);
+  if (isAdmin) return m.reply(`${emojis} Tu ya eres admin.`);
   try {
     await conn.groupParticipantsUpdate(m.chat, [m.sender], 'promote');
   await m.react(done)
-   m.reply(`⚡ Ya te di admin. `, m, rcanal);
+   m.reply(`${emojis} Ya te di admin. `, m, rcanal);
   } catch {
-    m.reply(`❌ Ocurrio un error.`, m, rcanal);
+    m.reply(`${emojis} Ocurrio un error.`, m, rcanal);
   }
 };
 handler.tags = ['owner'];
