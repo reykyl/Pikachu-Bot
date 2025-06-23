915b9774d1ca7a382630f48bee1d3b4eff55fec3
Deylin <deylinbo91@gmail.com>
2025-06-22 21:00:34 -0600

    Actualizar main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -119,7 +119,7 @@ global.icono = [
 'https://kirito-bot-md.vercel.app/catalogo.jpg'
 ].getRandom()
 
-global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: -1, newsletterName: channelRD.name, }, /*externalAdReply: { showAdAttribution: true, title: packname, body: dev, mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: icono, sourceUrl: redes,*/ mediaType: 1, renderLargerThumbnail: false }, }, }
+global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: -1, newsletterName: channelRD.name, }, /*externalAdReply: { showAdAttribution: true, title: packname, body: dev, mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: icono, sourceUrl: redes,*/ mediaType: 1, renderLargerThumbnail: false }, },
 
 
 
