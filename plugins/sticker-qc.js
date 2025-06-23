bacd0e9b1d3fd0f34d5923392035dd3c5b6e633d
Deylin <deylinbo91@gmail.com>
2025-06-22 21:41:03 -0600

    Actualizar sticker-qc.js

--- plugins/sticker-qc.js
+++ plugins/sticker-qc.js
@@ -10,12 +10,12 @@ if (args.length >= 1) {
 text = args.slice(0).join(" ");
 } else if (m.quoted && m.quoted.text) {
 text = m.quoted.text;
-} else return conn.reply(m.chat, `*ᰔᩚ ( ᜊ°-° )ᜊ  .ᐟ .ᐟ 🧃* *_Usa correctamente el comando entrenador.⚡_*`, m, rcanal);
-if (!text) return conn.reply(m.chat, `*ᰔᩚ ( ᜊ°-° )ᜊ  .ᐟ .ᐟ 🧃* *_Usa correctamente el comando entrenador.⚡_*`, m, rcanal);
+} else return conn.reply(m.chat, `*ᰔᩚ ( ᜊ°-° )ᜊ  .ᐟ .ᐟ 🧃* *_Usa correctamente el comando entrenador.⚡_*`, m, fake);
+if (!text) return conn.reply(m.chat, `*ᰔᩚ ( ᜊ°-° )ᜊ  .ᐟ .ᐟ 🧃* *_Usa correctamente el comando entrenador.⚡_*`, m, fake);
 const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender; 
 const mentionRegex = new RegExp(`@${who.split('@')[0].replace(/[.*+?^${}()|[\]\\]/g, '\\$&')}\\s*`, 'g');
 const mishi = text.replace(mentionRegex, '');
-if (mishi.length > 40) return conn.reply(m.chat, `*ᰔᩚ ( ᜊ°-° )ᜊ  .ᐟ .ᐟ 🧃* _*Entrenador! El texto no puede tener más de 30 palabras.*_`, m, rcanal);
+if (mishi.length > 40) return conn.reply(m.chat, `*ᰔᩚ ( ᜊ°-° )ᜊ  .ᐟ .ᐟ 🧃* _*Entrenador! El texto no puede tener más de 30 palabras.*_`, m, fake);
 const pp = await conn.profilePictureUrl(who).catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
 const nombre = await conn.getName(who)
 const obj = {"type": "quote", "format": "png", "backgroundColor": "#000000", "width": 512, "height": 768, "scale": 2, "messages": [{"entities": [], "avatar": true, "from": {"id": 1, "name": `${who?.name || nombre}`, "photo": {url: `${pp}`}}, "text": mishi, "replyMessage": {}}]};
