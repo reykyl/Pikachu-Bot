98004c99687848edc67b9341385c8b9498e799e2
Deylin <deylinbo91@gmail.com>
2025-06-23 19:48:19 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text }) => {
   if (!text) return m.reply('❗Escribe el texto que quieres que sea copiable.\nEjemplo: *.copiar Hola mundo*');
-  const bloqueCodigo = ['```', text, '```'].join('\n');
+  const bloqueCodigo = ['[[[', text, ']]]'].join('\n');
 
   await conn.sendMessage(m.chat, {
     text: bloqueCodigo
