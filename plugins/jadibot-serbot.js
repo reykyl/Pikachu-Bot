3b3aba94f1789244b6016d9fafea7d7750c76982
Deylin <deylinbo91@gmail.com>
2025-06-23 22:01:17 -0600

    Actualizar jadibot-serbot.js

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -211,7 +211,9 @@ txtCode = await conn.sendMessage(m.chat, {
     caption: rtx2,
     quoted: m
 });
-codeBot = await conn.reply(m.chat, `Aquí está tu código:\n\`\`\`\n${secret}\n\`\`\``, m);
+codeBot = await conn.reply(m.chat, `Aquí está tu código:
+
+```${secret}````, m);
 //} else {
 //txtCode = await conn.sendButton(m.chat, rtx2.trim(), wm, null, [], secret, null, m) 
 //}
