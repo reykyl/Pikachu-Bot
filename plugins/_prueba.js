7f55bdfbd756c3be472fe3ab6e00d24900e71d13
Deylin <deylinbo91@gmail.com>
2025-06-23 21:28:59 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -1,6 +1,6 @@
 const handler = async (m, { conn }) => {
     const codigoACopiar = 'DESC20'; // Aquí pones el código que quieres que se copie
-    const mensajeConCodigo = `Aquí está tu código:\n\`\`\`\n${codigoACopiar}\n\`\`\``;
+    const mensajeConCodigo = `Aquí está tu código:\n\`\`\`\n{{codigoACopiar}}\n\`\`\``;
 
     await conn.sendMessage(m.chat, { text: mensajeConCodigo }, { quoted: m });
 };
