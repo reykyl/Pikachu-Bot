26c7bd08362a9fdfe76322b4a3740d4914d4b94e
Deylin <deylinbo91@gmail.com>
2025-06-23 21:40:45 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -1,9 +1,11 @@
 const handler = async (m, { conn }) => {
     const codigoACopiar = 'PRUEBA_COPIAR_XYZ'; // Usa un código diferente para asegurar que es un mensaje nuevo
-    const mensajeConFormatoDeCodigo = `Aquí está tu código:\n\`\`\`\n${codigoACopiar}\n\`\`\``;
+    const mensajeConFormatoDeCodigo = `Aquí está tu código:`;
+    const code = `\`\`\`\n${codigoACopiar}\n\`\`\``;
 
     await conn.sendMessage(m.chat, {
         text: mensajeConFormatoDeCodigo
+        code: code
     }, { quoted: m });
 };
 
