2a3bc93a7766589bca6444d792f61dbd65e42a50
Deylin <deylinbo91@gmail.com>
2025-06-23 21:36:18 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -1,8 +1,12 @@
 const handler = async (m, { conn }) => {
-    const codigoACopiar = 'DESC20';
-const mensajeConCodigo = `Aquí está tu código:\n\`\`\`\n${codigoACopiar}\n\`\`\``;
-await conn.sendMessage(m.chat, { text: mensajeConCodigo }, { quoted: m });
+    const codigoACopiar = 'DESC20'; // El texto que quieres que el usuario pueda copiar
+    // Formatea el mensaje para que WhatsApp lo reconozca como un bloque de código
+    const mensajeConFormatoDeCodigo = `Aquí está tu código:\n\`\`\`\n${codigoACopiar}\n\`\`\``;
+
+    await conn.sendMessage(m.chat, {
+        text: mensajeConFormatoDeCodigo
+    }, { quoted: m }); // Envía el mensaje al chat
 };
 
-handler.command = ['h']; // O el comando que prefieras
+handler.command = ['h']; // O el comando que uses para activarlo
 export default handler;
