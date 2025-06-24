a55dab1864ab97bcdbb677d85a8a3865090bb309
Deylin <deylinbo91@gmail.com>
2025-06-23 20:13:38 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -1,26 +1,27 @@
-
-const handler = async (m, { conn}) => {
-  const codigo = `/* Código creado por Deylin...\nhttps://github.com/deylin-eliac */`;
-
+const handler = async (m, { conn }) => {
   const sections = [
     {
       title: "Copia este código",
       rows: [
-        { title: "📋 Copiar Código", description: "Toca aquí para copiar manualmente", rowId: `.copiarcodigo`}
+        {
+          title: "📋 Copiar Código",
+          description: "Toca aquí para ver y copiar el código",
+          rowId: ".copiarcodigo"
+        }
       ]
-}
+    }
   ];
 
   const listMessage = {
-    text: "Presiona para ver el código que puedes copiar:",
+    text: "Presiona el botón para ver el código que puedes copiar:",
     footer: "by Deylin-eliac",
     title: "🧾 Copiar código al portapapeles",
     buttonText: "Ver código",
     sections
+  };
+
+  await conn.sendMessage(m.chat, listMessage, { quoted: m });
 };
 
-  await conn.sendMessage(m.chat, listMessage, { quoted: m});
-};
-
-handler.command = /^copiarcodigo$/i;
-
+handler.command = ['h'];
+export default handler;
