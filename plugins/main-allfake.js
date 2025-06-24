812f9d42f9620e4f7643eb203b13fc795896acde
Deylin <deylinbo91@gmail.com>
2025-06-23 22:24:10 -0600

    Actualizar main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -72,6 +72,60 @@ global.waitt = global.wait
 global.waittt = global.wait
 global.waitttt = global.wait
 
+global.QR = let rtx = `
+╔══════════════════════════╗
+║ ⚡🐭  P I K A C H U   B O T  ⚡ 
+╠══════════════════════════╣
+║   ╭───(⚡◕ᴥ◕⚡)───╮         
+║   │  P I K A   │ P I K A  
+║   │   C H U !  │ C H U !  
+║   ╰─────────────╯         
+╠══════════════════════════╣
+║ 📡  Sub-Bot – Modo QR        
+╟──────────────────────────╢
+║ ⟿ ¡Pika Pika! Escanea este  
+║    código QR con otro       
+║    dispositivo o desde PC   
+║    para ser un *Sub-Bot*    
+║                            
+║ ➥ ❶ Toca ⋮ (tres rayitos)   
+║ ➥ ❷ Selecciona “Dispositivos
+║       vinculados”           
+║ ➥ ❸ Escanea y conéctate al 
+║       poder eléctrico ⚡     
+╟──────────────────────────╢
+║ ⚠  Expira en ❺❹ seg.        
+║   ¡No dejes que te atrape   
+║     la sobrecarga!          
+╚══════════════════════════╝`;
+
+global.CODE = `
+╔══════════════════════════╗
+║ ✨🐭  P I K A C H U   B O T  ✨ 
+╠══════════════════════════╣
+║   ╭───(⚡◕ᴥ◕⚡)───╮         
+║   │  P I K A   │ C H U !  
+║   │   C O D E  │   ⚡      
+║   ╰─────────────╯         
+╠══════════════════════════╣
+║ 🛠️  Sub-Bot – Modo Código    
+╟──────────────────────────╢
+║ ⟿ Usa este código para un   
+║   irte con la fuerza        
+║    eléctrica de Pikachu ⚡   
+║                            
+║ ➥ ❶ Abre ⋮ (tres rayitos)   
+║ ➥ ❷ “Dispositivos vinculados”
+║ ➥ ❸ Vincular con número     
+║ ➥ ❹ Ingresa el código ¡y    
+║       Pikaaa! Ya eres parte 
+║       del equipo eléctrico  
+╟──────────────────────────╢
+║ ⚠  Si ya tienes otra sesión 
+║    abierta, desconecta para 
+║    evitar sobrecarga ⚡      
+╚══════════════════════════╝`;
+
 //Enlaces
 var canal = 'https://whatsapp.com/channel/0029VawF8fBBvvsktcInIz3m'  
 let canal2 = 'https://whatsapp.com/channel/0029VawF8fBBvvsktcInIz3m'
