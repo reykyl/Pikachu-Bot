d372ac92c729559d8818eac80144d447178bc561
Deylin <deylinbo91@gmail.com>
2025-06-24 20:56:42 -0600

    Actualizar owner-saveplugin.js

--- plugins/owner-saveplugin.js
+++ plugins/owner-saveplugin.js
@@ -2,18 +2,18 @@ import fs from 'fs';
 
 let handler = async (m, { text, usedPrefix, command }) => {
     if (!text) {
-        return m.reply(`${emoji} Por favor, ingrese el nombre del plugin.`);
+        return m.reply(`${emojis} Por favor, ingrese el nombre del plugin.`);
     }
 
     if (!m.quoted || !m.quoted.text) {
-        return m.reply(`${emoji2} Responda al mensaje con el contenido del plugin.`);
+        return m.reply(`${emojis} Responda al mensaje con el contenido del plugin.`);
     }
 
     const ruta = `plugins/${text}.js`;
 
     try {
         await fs.writeFileSync(ruta, m.quoted.text);
-        m.reply(`${emoji} Guardando plugin en ${ruta}`);
+        m.reply(`${emojis} Guardando plugin en ${ruta}`);
     } catch (error) {
         m.reply(`${msm} Ocurrió un error al guardar el plugin: ${error.message}`);
     }
