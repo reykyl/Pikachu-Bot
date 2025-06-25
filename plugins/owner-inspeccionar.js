a8eeea34b8d951b5daf280b77fe47a6374837070
Deylin <deylinbo91@gmail.com>
2025-06-24 20:58:31 -0600

    Actualizar owner-inspeccionar.js

--- plugins/owner-inspeccionar.js
+++ plugins/owner-inspeccionar.js
@@ -33,7 +33,7 @@ let pp, ch, q, mime, buffer, media, inviteUrlch, imageBuffer
 switch (true) {     
 case isCommand1:
 let inviteCode
-if (!text) return await conn.reply(m.chat, `${emoji} Ingrese el enlace de grupo/comunidad o canal.`, m)
+if (!text) return await conn.reply(m.chat, `${emojis} Ingrese el enlace de grupo/comunidad o canal.`, m)
 const MetadataGroupInfo = async (res, isInviteInfo = false) => {
 let nameCommunity = "no pertenece a ninguna Comunidad"
 let groupPicture = "No se pudo obtener"
