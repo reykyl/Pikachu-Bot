e7fb0265406b85809cfa090367d28889b43cad05
Deylin <deylinbo91@gmail.com>
2025-06-26 16:59:28 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -1,8 +1,8 @@
 
 
 const handler = async (m, { conn, text, usedPrefix, command }) => {
-return conn.reply(m.chat, ` hola XD`, m, rcanal)},
-
+    return conn.reply(m.chat, "*Ｏ(≧∇≦)Ｏ🧃* *Pikachu-Bot* | Dime el nombre de la canción que estás buscando, ¡Pika!", m, rcanal)
+};
  handler.command = ['xd'];
 
 export default handler;
