33c704601f048cbb290ed1b306b5b4bd1e5735ec
Deylin <deylinbo91@gmail.com>
2025-06-26 16:58:17 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -1,7 +1,7 @@
 
 
 const handler = async (m, { conn, text, usedPrefix, command }) => {
-conn.reply(m.chat, ` hola XD`, m, rcanal)},
+returt conn.reply(m.chat, ` hola XD`, m, rcanal)},
 
  handler.command = ['xd'];
 
