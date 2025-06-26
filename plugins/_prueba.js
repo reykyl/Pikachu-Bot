1d9dcfffea77fa4d0abec4bfb921a5b45c91120c
Deylin <deylinbo91@gmail.com>
2025-06-26 17:04:45 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -1,8 +1,16 @@
+let handler = async (m, { conn }) => {
+  await conn.reply(m.chat, 'Hola ⚡ Cómo estás', m)
+}
+
+handler.command = ['hola'] 
+
+export default handler
 
 
-const handler = async (m, { conn, text, usedPrefix, command }) => {
+// Deylin 
+/*const handler = async (m, { conn, text, usedPrefix, command }) => {
     return conn.reply(m.chat, `Hola XD`, m, rcanal)
 };
  handler.command = ['xd'];
 
-export default handler;
+export default handler;*/
