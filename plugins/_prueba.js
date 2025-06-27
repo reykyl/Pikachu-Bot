9cfb7d9f018c20189c270ca40655565741ce0075
Deylin <deylinbo91@gmail.com>
2025-06-26 18:13:02 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -3,21 +3,16 @@ import fetch from 'node-fetch';
 let handler = async (m, { conn, usedPrefix, command }) => {
   try {
     const res = await fetch('https://g-mini-ia.vercel.app/api/nsfw');
-        const json = await res.json();
-    const meme = json.url;
+    const json = await res.json();
 
-
-    if (!posts.length) {
+    if (!json || !json.url) {
       return conn.reply(m.chat, '⚠️ No se encontró contenido NSFW por ahora.', m);
     }
 
-    
-    const post = posts[Math.floor(Math.random() * posts.length)].data;
     const image = json.url;
     const title = json.type;
-    const author = post.author;
 
-    await conn.sendFile(m.chat, image, 'meme.jpg', m);
+    await conn.sendFile(m.chat, image, 'nsfw.jpg', `🔞 Tipo: *${title}*`, m);
   } catch (e) {
     console.error(e);
     conn.reply(m.chat, '❌ Hubo un error al obtener el contenido NSFW.', m);
@@ -28,40 +23,4 @@ handler.command = ['nsfw2'];
 handler.tags = ['nsfw'];
 handler.help = ['nsfw'];
 
-
 export default handler;
-
-
-
-//🎰
-/*var handler = async (m, { conn }) => 
-{
-  await conn.reply(m.chat, `hola soy bajo perfil🐉`, m, rcanal)
-}
-
-handler.command = ['soy'] 
-
-export default handler*/
-
-
-//@⁨~NEOTOKIO⁩
-/*let handler = async (m, { conn }) => {
-  await conn.reply(m.chat, 'Hola ⚡ Cómo estás', m)
-}
-
-handler.command = ['hola'] 
-
-export default handler*/
-
-
-// Deylin 
-/*const handler = async (m, { conn, text, usedPrefix, command }) => {
-
-const deve = 'Deylin'
-const mi = 'Pikachu-bot'
-    return conn.reply(m.chat, `Hola soy: ${mi} fuí desarrollado por: ${deve} 
-visita: ${redes}`, m, rcanal)
-};
- handler.command = ['xd'];
-
-export default handler;*/
