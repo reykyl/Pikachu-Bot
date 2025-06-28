adf89c4ebb8c5e6099d054f3c2bc573ed842596d
Deylin <deylinbo91@gmail.com>
2025-06-28 14:35:18 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -6,7 +6,7 @@
  *  My Youtube : https://youtube.com/@luaserofc
 */
 
-import yts from 'yt-search';
+/*import yts from 'yt-search';
 
 let handler = async (m, { conn, command, text, usedPrefix }) => {
   if (!text) throw ` 🦄Use example *${usedPrefix + command}* Somewhere Only We Know`;
@@ -30,4 +30,25 @@ handler.help = ['play'].map((v) => v + ' <query>')
 handler.tags = ['downloader']
 handler.command = ['play', 'song']
 
+export default handler*/
+
+
+let handler = async (m, { conn }) => {
+  const play = `🎧 *Reproduciendo música...*\n\nSelecciona una opción:`
+  const thumbnail = 'https://telegra.ph/file/2c3b0ffac0e7267818023.jpg' // Puedes usar una imagen válida
+
+  await conn.sendButton2(
+    m.chat, // ID del chat
+    play,   // Texto del mensaje
+    thumbnail, // Imagen (URL o buffer)
+    [
+      ['🎶 Reproducir', 'hola'] // Botón que ejecuta el comando "hola"
+    ],
+    null, // ID de mensaje citado
+    null, // Opciones adicionales
+    m     // Objeto del mensaje original
+  )
+}
+
+handler.command = /^boton$/i
 export default handler
