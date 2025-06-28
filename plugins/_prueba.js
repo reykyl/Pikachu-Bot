73e91aa5ad2e317d619f00a7128da285bc85b11c
Deylin <deylinbo91@gmail.com>
2025-06-28 16:10:09 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -2,9 +2,9 @@
 
 let handler = async (m, { conn, command }) => {
   const TEXTO_GG = `🔐 Tu código de vinculación es:\nGAOK-IG36`;
-  const FOTOTETA = 'Kirito-Bot'; // Pie de mensaje
-  const URLIMG = 'https://i.imgur.com/AaJzNHz.jpeg'; // Imagen de portada
-  const COSACOPY = 'GAOK-IG36'; // Esto será el texto que aparece como 'copiable'
+  const FOTOTETA = 'Kirito-Bot'; 
+  const URLIMG = 'https://raw.githubusercontent.com/Deylin-Eliac/Pikachu-Bot/main/src/pika.jpg';
+  const COSACOPY = 'GAOK-IG36'; 
 
   await bot.sendButton2(m.chat, TEXTO_GG, FOTOTETA, URLIMG, [], COSACOPY, null, m);
 };
