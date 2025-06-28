975bb19b0b14d111017c505aaf00d54315415d54
Deylin <deylinbo91@gmail.com>
2025-06-28 16:06:36 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -3,14 +3,14 @@ let handler = async (m, { conn, command, usedPrefix }) => {
   const secret = generarCodigo() // función que genera el código, por ejemplo: GAOK-IG36
   const texto = `🔐 *Tu código de vinculación es:*`;
   const footer = `📡 Usa este código para vincular tu sub-bot`;
-  const image = 'https://i.imgur.com/AaJzNHz.jpeg'; // Puedes poner cualquier imagen
+  //const image = 'https://i.imgur.com/AaJzNHz.jpeg'; // Puedes poner cualquier imagen
   const buttons = [
     { buttonId: `${usedPrefix}menu`, buttonText: { displayText: "📜 Menú" }, type: 1 },
     { buttonId: `${usedPrefix}info`, buttonText: { displayText: "ℹ️ Información" }, type: 1 }
   ];
   const copyText = `${secret}`; // Texto final que aparece como si fuera 'copiable'
 
-  await conn.sendButton2(m.chat, texto, footer, image, buttons, copyText, null, m);
+  await conn.sendButton2(m.chat, texto, footer, buttons, copyText, null, m);
 };
 
 // Generador de código aleatorio
