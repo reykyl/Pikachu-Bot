9baa8753209a1334c08eaac8f55b1edd22cb3aef
Deylin <deylinbo91@gmail.com>
2025-06-28 18:13:13 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -2,17 +2,7 @@ let handler = async (m, { conn }) => {
   const textoCopia = '🔒 Este es tu código de verificación: *123456*';
   const footer = 'Presiona el botón para copiar el código';
 
-  await conn.sendButton2(
-    m.chat,
-    textoCopia,
-    [], // no imagen o URL
-    footer,
-    [
-      ['📋 Copiar', 'Copiar'] // Esto solo manda "Copiar" al chat cuando se pulsa
-    ],
-    null,
-    m
-  );
+  await conn.sendButton2(m.chat, textoCopia, [], footer, null, m);
 };
 
 handler.command = ['cop'];
