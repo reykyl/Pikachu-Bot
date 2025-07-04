baf144ff81ecfe4a4acfd0b9f077638647672f1b
Deylin <deylinbo91@gmail.com>
2025-07-03 22:37:30 -0600

    Crear _sell.js

---
+++ plugins/_sell.js
@@ -0,0 +1,33 @@
+let handler = async (m, { conn }) => {
+  const jid = m.chat;
+
+  try {
+    const productMessage = {
+      product: {
+        productImage: {
+          url: 'https://files.catbox.moe/b0woxx.jpg'
+        },
+        title: "Pikachu-bot - Development by Deylin ",
+        description: " Alquila o compra Pikachu Bot para tus grupos.",
+        currencyCode: "USD",
+        priceAmount1000: 5000, // 5.00 USD
+       // retailerId: "1466", 
+        productId: "24502048122733040", 
+        productImageCount: 1,
+      },
+      businessOwnerJid: "50433191934@s.whatsapp.net" 
+    };
+
+    await conn.sendMessage(jid, productMessage, { messageType: 'product' });
+  } catch (error) {
+    console.error('Error enviando catálogo:', error);
+    conn.reply(jid, '❌ No se pudo enviar el catálogo. Verifica que el productId y el número Business sean correctos.', m);
+  }
+};
+
+handler.help = ['comprar', 'producto', 'compra'];
+handler.command = ['comprar', 'producto', 'compra'];
+handler.tags = ['ventas'];
+handler.register = true;
+
+export default handler;
