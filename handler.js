5299e8aac9f57477668e3e41d3908fc2968f5499
Deylin <deylinbo91@gmail.com>
2025-07-05 00:19:36 -0600

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -269,21 +269,17 @@ m.exp += Math.ceil(Math.random() * 10)
 let usedPrefix
 
 const groupMetadata = (m.isGroup ? ((conn.chats[m.chat] || {}).metadata || await this.groupMetadata(m.chat).catch(_ => null)) : {}) || {}
-//const participants = (m.isGroup ? groupMetadata.participants : []) || []
+const participants = (m.isGroup ? groupMetadata.participants : []) || []
 let numBot = false
 if (conn.user && conn.user.lid) {
   numBot = conn.user.lid.replace(/:.*/, '')
 }
-let participants = m.isGroup ? (await conn.groupMetadata(m.chat)).participants : []
-
 const detectwhat2 = m.sender.includes('@lid') ? `${numBot}@lid` : conn.user.jid
-const user = m.isGroup ? participants.find(u => conn.decodeJid(u.id) === m.sender) || {} : {}
-const bot = m.isGroup ? participants.find(u => conn.decodeJid(u.id) === detectwhat2) || {} : {}
-
-const isRAdmin = user?.admin === 'superadmin'
-const isAdmin = isRAdmin || user?.admin === 'admin'
-const isBotAdmin = bot?.admin === 'admin' || bot?.admin === 'superadmin'
-
+const user = (m.isGroup ? participants.find(u => conn.decodeJid(u.id) === m.sender) : {}) || {}
+const bot = (m.isGroup ? participants.find(u => conn.decodeJid(u.id) == detectwhat2) : {}) || {}
+const isRAdmin = user?.admin == 'superadmin' || false
+const isAdmin = isRAdmin || user?.admin == 'admin' || false //user admins? 
+const isBotAdmin = bot?.admin || false //Detecta sin el bot es admin
 m.isWABusiness = global.conn.authState?.creds?.platform === 'smba' || global.conn.authState?.creds?.platform === 'smbi'
 m.isChannel = m.chat.includes('@newsletter') || m.sender.includes('@newsletter')
 
