7fe00c36bb2111956c8d0e90c774d0bc83073243
Deylin <deylinbo91@gmail.com>
2025-07-04 19:02:34 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -3,22 +3,41 @@
 // ➤ No quites créditos
 
 let handler = async (m, { conn }) => {
-  const url = 'https://github.com/Deylin-Eliac' // 🔗 Cambia esta URL si deseas
+  const canal = 'https://whatsapp.com/channel/0029VawF8fBBvvsktcInIz3m'
 
   await conn.sendMessage(m.chat, {
-    text: '🔗 *Haz clic en el botón para abrir la URL:*',
-    footer: 'Pikachu-Bot • by Deylin',
-    templateButtons: [
+    text: '🪄 *Canal oficial de Pikachu-Bot*',
+    footer: 'Síguelo para más actualizaciones',
+    buttons: [
       {
-        index: 1,
-        urlButton: {
-          displayText: '🌐 Abrir GitHub',
-          url: url
-        }
+        name: 'cta_url',
+        buttonParamsJson: JSON.stringify({
+          display_text: '✐ Canal oficial',
+          url: canal,
+          merchant_url: canal
+        })
       }
-    ]
+    ],
+    headerType: 1,
+    contextInfo: {
+      forwardedNewsletterMessageInfo: {
+        newsletterJid: '120363217179176783@newsletter',
+        newsletterName: 'Pikachu-Bot Oficial 🧪',
+        serverMessageId: '',
+      },
+      externalAdReply: {
+        title: 'Pikachu-Bot Oficial',
+        body: 'Únete al canal y entérate de todo',
+        mediaUrl: canal,
+        sourceUrl: canal,
+        thumbnailUrl: 'https://files.catbox.moe/b0woxx.jpg',
+        mediaType: 1,
+        renderLargerThumbnail: true,
+        showAdAttribution: true
+      }
+    }
   }, { quoted: m })
 }
 
-handler.command = ['a'] // comando .a
+handler.command = ['a']
 export default handler
