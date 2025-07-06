cc26e8a13c8689a675619ca49108953003400863
Deylin <deylinbo91@gmail.com>
2025-07-05 22:46:21 -0600

    Actualizar _apis.js

--- plugins/_apis.js
+++ plugins/_apis.js
@@ -1,6 +1,6 @@
 import { generateWAMessageFromContent, proto } from '@whiskeysockets/baileys'
 
-let handler = async (m, { conn }) => {
+/*let handler = async (m, { conn }) => {
   const text = `*🔧 APIs desarrolladas por Deylin*
 
 *🌐 Prueba APIs directamente aquí:* 
@@ -80,4 +80,43 @@ handler.command = ['api', 'apis', 'servicios']
 handler.tags = ['main']
 handler.help = ['api', 'apis', 'servicios']
 
+export default handler*/
+
+const handler = async (m, { conn, command }) => {
+  const texto = `✨ Pulsa el botón para unirte al canal oficial`.trim();
+
+  const messageContent = {
+    viewOnceMessage: {
+      message: {
+        messageContextInfo: {
+          deviceListMetadata: {},
+          deviceListMetadataVersion: 2
+        },
+        interactiveMessage: proto.Message.InteractiveMessage.create({
+          body: proto.Message.InteractiveMessage.Body.create({ text: texto }),
+          footer: proto.Message.InteractiveMessage.Footer.create({ text: 'Pikachu Bot by Deylin' }),
+          header: proto.Message.InteractiveMessage.Header.create({ hasMediaAttachment: false }),
+          nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
+            buttons: [
+              {
+                name: 'cta_url',
+                buttonParamsJson: JSON.stringify({
+                  display_text: '✐ Canal oficial',
+                  url: 'https://whatsapp.com/channel/0029VawF8fBBvvsktcInIz3m',
+                  merchant_url: 'https://whatsapp.com/channel/0029VawF8fBBvvsktcInIz3m'
+                })
+              }
+            ]
+          })
+        })
+      }
+    }
+  }
+
+  const msg = generateWAMessageFromContent(m.chat, messageContent, { quoted: m });
+  await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id });
+}
+
+handler.command = ['can']
+handler.register = true
 export default handler
