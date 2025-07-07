679be97a65a0a8e2ddec0fdea37bcbc5fefb3a58
Deylin <deylinbo91@gmail.com>
2025-07-06 19:17:30 -0600

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -5,6 +5,7 @@ import path, { join } from 'path'
 import { unwatchFile, watchFile } from 'fs'
 import chalk from 'chalk'
 import fetch from 'node-fetch'
+import getMensajeSistema from './lib/msmwarning.js'
 
 const { proto } = (await import('@whiskeysockets/baileys')).default
 const isNumber = x => typeof x === 'number' && !isNaN(x)
