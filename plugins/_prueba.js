c0ddb88d15a0722f7ecf2b153c7c645d59bb2cf1
Deylin <deylinbo91@gmail.com>
2025-07-10 14:25:53 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -2,14 +2,15 @@ import { generateWAMessageFromContent, proto } from '@whiskeysockets/baileys';
 
 const handler = async (m, { conn }) => {
   await conn.sendMessage(m.chat, {
-  text: 'Gracias por usar el bot. ¿Qué deseas hacer ahora?',
-  footer: '¡Hasta pronto!',
-  buttons: [
-    { buttonId: '#owner', buttonText: { displayText: 'Creador ' }, type: 1 },
-    { buttonId: '#menu', buttonText: { displayText: 'Menu 📚' }, type: 1 }
-  ],
-  headerType: 1
-});
+    text: 'Gracias por usar el bot. ¿Qué deseas hacer ahora?',
+    footer: '¡Hasta pronto!',
+    buttons: [
+      { buttonId: '#owner', buttonText: { displayText: 'Creador 🍟' }, type: 1 },
+      { buttonId: '#menu', buttonText: { displayText: 'Menu 📚' }, type: 1 }
+    ],
+    headerType: 1
+  });
+};
 
 handler.help = ['m'];
 handler.tags = ['tools'];
