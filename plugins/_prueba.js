90aa9e77db81cb5f28a2ff688d16d103abc0385b
Deylin <deylinbo91@gmail.com>
2025-07-09 22:52:51 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -1,31 +1,23 @@
-import fetch from 'node-fetch';
-
-let handler = async (m, { conn, text, args, usedPrefix, command }) => {
-  if (!text) {
-    return m.reply(`⚠️ Uso incorrecto\n\nEjemplo:\n${usedPrefix + command} https://api.example.com/datos`);
-  }
-
-  try {
-    const res = await fetch(text);
-    const contentType = res.headers.get('content-type') || '';
-
-    
-    if (contentType.includes('application/json')) {
-      const json = await res.json();
-      return m.reply(`✅ *Respuesta JSON:*\n\n` + JSON.stringify(json, null, 2));
-    }
-
-    
-    const textRes = await res.text();
-    return m.reply(`✅ *Respuesta de la API:*\n\n${textRes}`);
-  } catch (err) {
-    console.error(err);
-    return m.reply(`❌ Ocurrió un error al acceder a la API:\n\n${err.message}`);
-  }
+const handler = async (m, { conn }) => {
+  await conn.sendMessage(m.chat, { 
+    text: 'prueba:',
+    buttons: [
+      {
+        buttonId: '.owner',
+        buttonText: { displayText: '.creador' },
+      },
+      {
+        buttonId: '.menu',
+        buttonText: { displayText: '.menu' },
+      },
+       ],
+    footer: '¡ - BOT!',
+    viewOnce: true,
+  }, { quoted: m });
 };
 
-handler.help = ['probarapi <url>'];
 handler.tags = ['tools'];
-handler.command = /^probarapi$/i;
+handler.help = ['webinfo'];
+handler.command = ['m'];
 
 export default handler;
