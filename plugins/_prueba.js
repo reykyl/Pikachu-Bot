5a0fb217091db8fad0cc73c984f5e67528d7413f
Deylin <deylinbo91@gmail.com>
2025-07-10 14:24:52 -0600

    Actualizar _prueba.js

--- plugins/_prueba.js
+++ plugins/_prueba.js
@@ -1,52 +1,15 @@
 import { generateWAMessageFromContent, proto } from '@whiskeysockets/baileys';
 
 const handler = async (m, { conn }) => {
-  const farewellMsg = 'Gracias por haber usado el bot. ¡Nos vemos pronto!';
-
-  const msg = generateWAMessageFromContent(m.chat, {
-    viewOnceMessage: {
-      message: {
-        messageContextInfo: {
-          deviceListMetadata: {},
-          deviceListMetadataVersion: 2
-        },
-        interactiveMessage: proto.Message.InteractiveMessage.create({
-          body: proto.Message.InteractiveMessage.Body.create({
-            text: farewellMsg
-          }),
-          footer: proto.Message.InteractiveMessage.Footer.create({
-            text: '¡Hasta pronto!'
-          }),
-          header: proto.Message.InteractiveMessage.Header.create({
-            title: '',
-            subtitle: '',
-            hasMediaAttachment: false
-          }),
-          nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
-            buttons: [
-              {
-                name: 'quick_reply',
-                buttonParamsJson: JSON.stringify({
-                  display_text: 'Creador 🍟',
-                  id: '#owner'
-                })
-              },
-              {
-                name: 'quick_reply',
-                buttonParamsJson: JSON.stringify({
-                  display_text: 'Menu 📚',
-                  id: '#menu'
-                })
-              }
-            ]
-          })
-        })
-      }
-    }
-  }, {});
-
-  await conn.relayMessage(m.chat, msg.message, { messageId: m.key.id });
-};
+  await conn.sendMessage(m.chat, {
+  text: 'Gracias por usar el bot. ¿Qué deseas hacer ahora?',
+  footer: '¡Hasta pronto!',
+  buttons: [
+    { buttonId: '#owner', buttonText: { displayText: 'Creador ' }, type: 1 },
+    { buttonId: '#menu', buttonText: { displayText: 'Menu 📚' }, type: 1 }
+  ],
+  headerType: 1
+});
 
 handler.help = ['m'];
 handler.tags = ['tools'];
