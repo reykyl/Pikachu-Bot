a50c021c9c5aaeaf79f094ceaa90d195648183a4
Deylin <deylinbo91@gmail.com>
2025-07-13 00:46:56 -0600

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -269,7 +269,7 @@ const user = (m.isGroup ? participants.find(u => conn.decodeJid(u.id) === m.send
 const bot = (m.isGroup ? participants.find(u => conn.decodeJid(u.id) == detectwhat2) : {}) || {}
 const isRAdmin = user?.admin == 'superadmin' || false
 const isAdmin = isRAdmin || user?.admin == 'admin' || false //user admins? 
-const isBotAdmin = bot?.admin || null //Detecta sin el bot es admin
+const isBotAdmin = bot?.admin || isAdmin || false //Detecta sin el bot es admin
 m.isWABusiness = global.conn.authState?.creds?.platform === 'smba' || global.conn.authState?.creds?.platform === 'smbi'
 m.isChannel = m.chat.includes('@newsletter') || m.sender.includes('@newsletter')
 
